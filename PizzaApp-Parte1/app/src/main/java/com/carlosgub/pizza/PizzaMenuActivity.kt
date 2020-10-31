c50761134d7fc1bfa29fc3ebf69214c984547074
carlos gabriel <carlosgabriel@MacBook-Pro-de-carlos.local>
2020-10-31 14:57:21 -0500

    Agregar primera parte de PizzaApp y cambiar nombre del projecto terminado a terminado

---
+++ PizzaApp-Parte1/app/src/main/java/com/carlosgub/pizza/PizzaMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.carlosgub.pizza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PizzaMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pizza_menu_activity)
+    }
+}
